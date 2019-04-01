bbe4294ecd1234ad9cb22283291b4a1ff2b179c0
liuenci <154910381@qq.com>
2019-04-01 23:34:27 +0800

    update user catalog

--- src/main/java/com/liuenci/vblog/controller/CatalogController.java
+++ src/main/java/com/liuenci/vblog/controller/CatalogController.java
@@ -28,108 +28,117 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 分类 控制器.
+ *
+ * @author liuenci
  */
 @Controller
 @RequestMapping("/catalogs")
 public class CatalogController {
-	
-	@Autowired
-	private CatalogService catalogService;
-	
-	@Autowired
-	private UserDetailsService userDetailsService;
-	/**
-	 * 获取分类列表
-	 * @param blogId
-	 * @param model
-	 * @return
-	 */
-	@GetMapping
-	public String listComments(@RequestParam(value="username",required=true) String username, Model model) {
-		User user = (User)userDetailsService.loadUserByUsername(username);
-		List<Catalog> catalogs = catalogService.listCatalogs(user);
 
-		// 判断操作用户是否是分类的所有者
-		boolean isOwner = false;
-		
-		if (SecurityContextHolder.getContext().getAuthentication() !=null && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()
-				 &&  !SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString().equals("anonymousUser")) {
-			User principal = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal(); 
-			if (principal !=null && user.getUsername().equals(principal.getUsername())) {
-				isOwner = true;
-			} 
-		} 
-		
-		model.addAttribute("isCatalogsOwner", isOwner);
-		model.addAttribute("catalogs", catalogs);
-		return "/userspace/u :: #catalogRepleace";
-	}
-	/**
-	 * 发表分类
-	 * @return
-	 */
-	@PostMapping
-	@PreAuthorize("authentication.name.equals(#catalogVO.username)")// 指定用户才能操作方法
-	public ResponseEntity<Response> create(@RequestBody CatalogVO catalogVO) {
-		
-		String username = catalogVO.getUsername();
-		Catalog catalog = catalogVO.getCatalog();
-		
-		User user = (User)userDetailsService.loadUserByUsername(username);
-		
-		try {
-			catalog.setUser(user);
-			catalogService.saveCatalog(catalog);
-		} catch (ConstraintViolationException e)  {
-			return ResponseEntity.ok().body(new Response(false, ConstraintViolationExceptionHandler.getMessage(e)));
-		} catch (Exception e) {
-			return ResponseEntity.ok().body(new Response(false, e.getMessage()));
-		}
-		
-		return ResponseEntity.ok().body(new Response(true, "处理成功", null));
-	}
-	
-	/**
-	 * 删除分类
-	 * @return
-	 */
-	@DeleteMapping("/{id}")
-	@PreAuthorize("authentication.name.equals(#username)")  // 指定用户才能操作方法
-	public ResponseEntity<Response> delete(String username, @PathVariable("id") Long id) {
-		try {
-			catalogService.removeCatalog(id);
-		} catch (ConstraintViolationException e)  {
-			return ResponseEntity.ok().body(new Response(false, ConstraintViolationExceptionHandler.getMessage(e)));
-		} catch (Exception e) {
-			return ResponseEntity.ok().body(new Response(false, e.getMessage()));
-		}
-		
-		return ResponseEntity.ok().body(new Response(true, "处理成功", null));
-	}
-	
-	/**
-	 * 获取分类编辑界面
-	 * @param id
-	 * @param model
-	 * @return
-	 */
-	@GetMapping("/edit")
-	public String getCatalogEdit(Model model) {
-		Catalog catalog = new Catalog(null, null);
-		model.addAttribute("catalog",catalog);
-		return "/userspace/catalogedit";
-	}
-	/**
-	 * 根据 Id 获取分类信息
-	 * @param id
-	 * @param model
-	 * @return
-	 */
-	@GetMapping("/edit/{id}")
-	public String getCatalogById(@PathVariable("id") Long id, Model model) {
-		Catalog catalog = catalogService.getCatalogById(id);
-		model.addAttribute("catalog",catalog);
-		return "/userspace/catalogedit";
-	}
-	
+    @Autowired
+    private CatalogService catalogService;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    /**
+     * 获取分类列表
+     *
+     * @param model
+     * @return
+     */
+    @GetMapping
+    public String listComments(@RequestParam(value = "username", required = true) String username, Model model) {
+        User user = (User) userDetailsService.loadUserByUsername(username);
+        List<Catalog> catalogs = catalogService.listCatalogs(user);
+
+        // 判断操作用户是否是分类的所有者
+        boolean isOwner = false;
+
+        if (SecurityContextHolder.getContext().getAuthentication() != null && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()
+                && !SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString().equals("anonymousUser")) {
+            User principal = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if (principal != null && user.getUsername().equals(principal.getUsername())) {
+                isOwner = true;
+            }
+        }
+
+        model.addAttribute("isCatalogsOwner", isOwner);
+        model.addAttribute("catalogs", catalogs);
+        return "/userspace/u :: #catalogRepleace";
+    }
+
+    /**
+     * 发表分类
+     *
+     * @return
+     */
+    @PostMapping
+    @PreAuthorize("authentication.name.equals(#catalogVO.username)")// 指定用户才能操作方法
+    public ResponseEntity<Response> create(@RequestBody CatalogVO catalogVO) {
+
+        String username = catalogVO.getUsername();
+        Catalog catalog = catalogVO.getCatalog();
+
+        User user = (User) userDetailsService.loadUserByUsername(username);
+
+        try {
+            catalog.setUser(user);
+            catalogService.saveCatalog(catalog);
+        } catch (ConstraintViolationException e) {
+            return ResponseEntity.ok().body(new Response(false, ConstraintViolationExceptionHandler.getMessage(e)));
+        } catch (Exception e) {
+            return ResponseEntity.ok().body(new Response(false, e.getMessage()));
+        }
+
+        return ResponseEntity.ok().body(new Response(true, "处理成功", null));
+    }
+
+    /**
+     * 删除分类
+     * 指定用户才能操作方法
+     *
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    @PreAuthorize("authentication.name.equals(#username)")
+    public ResponseEntity<Response> delete(String username, @PathVariable("id") Long id) {
+        try {
+            catalogService.removeCatalog(id);
+        } catch (ConstraintViolationException e) {
+            return ResponseEntity.ok().body(new Response(false, ConstraintViolationExceptionHandler.getMessage(e)));
+        } catch (Exception e) {
+            return ResponseEntity.ok().body(new Response(false, e.getMessage()));
+        }
+
+        return ResponseEntity.ok().body(new Response(true, "处理成功", null));
+    }
+
+    /**
+     * 获取分类编辑界面
+     *
+     * @param model
+     * @return
+     */
+    @GetMapping("/edit")
+    public String getCatalogEdit(Model model) {
+        Catalog catalog = new Catalog(null, null);
+        model.addAttribute("catalog", catalog);
+        return "/userspace/catalogedit";
+    }
+
+    /**
+     * 根据 Id 获取分类信息
+     *
+     * @param id
+     * @param model
+     * @return
+     */
+    @GetMapping("/edit/{id}")
+    public String getCatalogById(@PathVariable("id") Long id, Model model) {
+        Catalog catalog = catalogService.getCatalogById(id);
+        model.addAttribute("catalog", catalog);
+        return "/userspace/catalogedit";
+    }
+
 }
