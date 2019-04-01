33cd704041cd9a0ee0223a1c672d4e3eb6684c1c
liuenci <154910381@qq.com>
2019-04-01 22:48:42 +0800

    login

--- src/main/java/com/liuenci/vblog/pojo/User.java
+++ src/main/java/com/liuenci/vblog/pojo/User.java
@@ -31,143 +31,143 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Entity // 实体
 public class User implements UserDetails, Serializable {
 
-	private static final long serialVersionUID = 1L;
-	
-	@Id // 主键
-	@GeneratedValue(strategy = GenerationType.IDENTITY) // 自增长策略
-	private Long id; // 用户的唯一标识
+    private static final long serialVersionUID = 1L;
 
-	@NotEmpty(message = "姓名不能为空")
-	@Size(min=2, max=20)
-	@Column(nullable = false, length = 20) // 映射为字段，值不能为空
-	private String name;
+    @Id // 主键
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 自增长策略
+    private Long id; // 用户的唯一标识
 
-	@NotEmpty(message = "邮箱不能为空")
-	@Size(max=50)
-	@Email(message= "邮箱格式不对" ) 
-	@Column(nullable = false, length = 50, unique = true)
-	private String email;
+    @NotEmpty(message = "姓名不能为空")
+    @Size(min = 2, max = 20)
+    @Column(nullable = false, length = 20) // 映射为字段，值不能为空
+    private String name;
 
-	@NotEmpty(message = "账号不能为空")
-	@Size(min=3, max=20)
-	@Column(nullable = false, length = 20, unique = true)
-	private String username; // 用户账号，用户登录时的唯一标识
+    @NotEmpty(message = "邮箱不能为空")
+    @Size(max = 50)
+    @Email(message = "邮箱格式不对")
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
 
-	@NotEmpty(message = "密码不能为空")
-	@Size(max=100)
-	@Column(length = 100)
-	private String password; // 登录时密码
-	
-	@Column(length = 200)
-	private String avatar; // 头像图片地址
+    @NotEmpty(message = "账号不能为空")
+    @Size(min = 3, max = 20)
+    @Column(nullable = false, length = 20, unique = true)
+    private String username; // 用户账号，用户登录时的唯一标识
 
-	@ManyToMany(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
-	@JoinTable(name = "user_authority", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), 
-		inverseJoinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id"))
-	private List<Authority> authorities;
+    @NotEmpty(message = "密码不能为空")
+    @Size(max = 100)
+    @Column(length = 100)
+    private String password; // 登录时密码
 
-	protected User() { // JPA 的规范要求无参构造函数；设为 protected 防止直接使用
-	}
+    @Column(length = 200)
+    private String avatar; // 头像图片地址
 
-	public User(String name, String email,String username,String password) {
-		this.name = name;
-		this.email = email;
-		this.username = username;
-		this.password = password;
-	}
+    @ManyToMany(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_authority", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id"))
+    private List<Authority> authorities;
 
-	public Long getId() {
-		return id;
-	}
+    protected User() { // JPA 的规范要求无参构造函数；设为 protected 防止直接使用
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public User(String name, String email, String username, String password) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		//  需将 List<Authority> 转成 List<SimpleGrantedAuthority>，否则前端拿不到角色列表名称
-		List<SimpleGrantedAuthority> simpleAuthorities = new ArrayList<>();
-		for(GrantedAuthority authority : this.authorities){
-			simpleAuthorities.add(new SimpleGrantedAuthority(authority.getAuthority()));
-		}
-		return simpleAuthorities;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setAuthorities(List<Authority> authorities) {
-		this.authorities = authorities;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	@Override
-	public String getUsername() {
-		return username;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        //  需将 List<Authority> 转成 List<SimpleGrantedAuthority>，否则前端拿不到角色列表名称
+        List<SimpleGrantedAuthority> simpleAuthorities = new ArrayList<>();
+        for (GrantedAuthority authority : this.authorities) {
+            simpleAuthorities.add(new SimpleGrantedAuthority(authority.getAuthority()));
+        }
+        return simpleAuthorities;
+    }
 
-	@Override
-	public String getPassword() {
-		return password;
-	}
+    public void setAuthorities(List<Authority> authorities) {
+        this.authorities = authorities;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    @Override
+    public String getUsername() {
+        return username;
+    }
 
-	public void setEncodePassword(String password) {
-		PasswordEncoder  encoder = new BCryptPasswordEncoder();
-		String encodePasswd = encoder.encode(password);
-		this.password = encodePasswd;
-	}
-	
-	public String getAvatar() {
-		return avatar;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setAvatar(String avatar) {
-		this.avatar = avatar;
-	}
+    @Override
+    public String getPassword() {
+        return password;
+    }
 
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
+    public void setEncodePassword(String password) {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodePasswd = encoder.encode(password);
+        this.password = encodePasswd;
+    }
 
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
+    public String getAvatar() {
+        return avatar;
+    }
 
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
 
-	@Override
-	public String toString() {
-		return String.format("User[id=%d, username='%s', name='%s', email='%s', password='%s']", id, username, name, email,
-				password);
-	}
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User[id=%d, username='%s', name='%s', email='%s', password='%s']", id, username, name, email,
+                password);
+    }
 }
